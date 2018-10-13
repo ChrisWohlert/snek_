706fd14d7a54aca22970d47c7c46c0ffae892ee6
ChrisWohlert <chriswohlert@hotmail.com>
2018-10-13 11:43:25 +0200

    can now draw a line

--- snek/app/Main.hs
+++ snek/app/Main.hs
@@ -12,5 +12,13 @@ main = do
 
 display :: DisplayCallback
 display = do
+    let color3f r g b = color $ Color3 r g (b :: GLfloat)
+        vertex3f x y z = vertex $ Vertex3 x y (z :: GLfloat)
     clear [ ColorBuffer ]
+    renderPrimitive Lines $ do
+      color3f 1 1 1
+      vertex3f (-1) (-1) 0
+      vertex3f 0.5 0.5 0
+      vertex3f 0.5 0.5 0
+      vertex3f (-0.5) 0.5 0
     flush
